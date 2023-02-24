35ee1c25f11cad0727db7a950dde30af714330ae
TheFakeKingIsTaken <125929023+TheFakeKingIsTaken@users.noreply.github.com>
2023-02-24 16:49:37 -0500

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,1 +1,8 @@
+--skiddos get your skiddoing hands away from here before i pull out two glock 18
+
+
+--[checker]
+if 1 == 1 then -- for now no other games
+     loadstring(game:HttpGet("https://github.com/TheFakeKingIsTaken/Atom-Hub/blob/main/Games/Universal.lua", true))()
+ end
 
