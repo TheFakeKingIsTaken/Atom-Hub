ce008323ce78093f14d3f2198c3e258a9c57744a
TheFakeKingIsTaken <125929023+TheFakeKingIsTaken@users.noreply.github.com>
2023-02-26 17:06:34 -0500

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -2,7 +2,9 @@
 
 
 --[checker]
-if game.PlaceId == 12413542851 then
+if game.PlaceId == 566399244 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/ElementalBattlegrounds.lua", true))()
+elseif game.PlaceId == 12413542851 then
      loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/DropKick.lua", true))()
 else
      loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/Universal.lua", true))()
