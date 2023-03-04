933503f0375fdef8bade33fbbd6de2e358f9db28
TheFakeKingIsTaken <125929023+TheFakeKingIsTaken@users.noreply.github.com>
2023-03-04 12:44:11 -0500

    Update Users.lua

--- Private/Users.lua
+++ Private/Users.lua
@@ -2,5 +2,6 @@
 --this is a list for private users
 
 return {
- "4D6C2BB5-2D04-4686-854A-31C4D2B6E330" -- TheFakeKing#9675
+ "4D6C2BB5-2D04-4686-854A-31C4D2B6E330", -- TheFakeKing#9675
+ "7c508ca5-0177-468d-942b-f9a66199edaa" --S1mplicity#5108
 }
