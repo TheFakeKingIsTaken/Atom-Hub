22d38a60a2c862c1ae54d7d2308459ed689915a5
TheFakeKingIsTaken <125929023+TheFakeKingIsTaken@users.noreply.github.com>
2023-03-04 12:08:24 -0500

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,18 +1,19 @@
 --skiddos get your skiddoing hands away from here before i pull out two glock 18
 
 
-local PrivateUser = nil
-
-
+local PrivateUser = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Private/Whitelist.lua", true))()
 
 
 --[checker]
-
-if game.PlaceId == 566399244 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/ElementalBattlegrounds.lua", true))()
-elseif game.PlaceId == 12413542851 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/DropKick.lua", true))()
+if PrivateUser then
+    if game.PlaceId == 566399244 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/ElementalBattlegrounds.lua", true))()
+    elseif game.PlaceId == 12413542851 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/DropKick.lua", true))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/Universal.lua", true))()
+    end
 else
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/Universal.lua", true))()
- end
-
+    -- for now nothing for private users
+    print("test")
+end
