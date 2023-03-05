e57f428b0fc5dbfe71eb1a5a1eb0451d5d785390
TheFakeKingIsTaken <125929023+TheFakeKingIsTaken@users.noreply.github.com>
2023-03-05 16:53:05 -0500

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -9,11 +9,11 @@ local hwidtable = loadstring(game:HttpGet("https://raw.githubusercontent.com/The
 
 for i,v in pairs(hwidtable) do
     if v == ID then
-        -- for now nothing for private users
+        
         if game.PlaceId == 566399244 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/ElementalBattlegrounds.lua", true))()
         elseif game.PlaceId == 12413542851 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/DropKick.lua", true))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Private/Games/DropKick.lua", true))()
         else 
             loadstring(game:HttpGet("https://raw.githubusercontent.com/TheFakeKingIsTaken/Atom-Hub/main/Games/Universal.lua", true))()
         end
